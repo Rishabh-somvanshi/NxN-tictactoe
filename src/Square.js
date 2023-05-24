8938a43e0c769fae5f98a81d37d267b4823c9b1a
Rishabh-somvanshi <rishabhsomvanshi87@gmail.com>
2023-05-24 13:34:20 +0530

    1 file modified

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Square({ value, onClick }) {
   return (
     <div onClick={onClick}>
+      <input type="text" value={value}>
       {value}
+      </input>
     </div>
   );
 }
